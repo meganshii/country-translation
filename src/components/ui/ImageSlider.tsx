8270a3e081a82fa6004d7fb6f05b6f400f1af302
kanha-9770 <app3.nessco@gmail.com>
2024-10-15 12:00:11 +0530

    adding fill attribute along with size to lcp based on screen

--- src/components/ui/ImageSlider.tsx
+++ src/components/ui/ImageSlider.tsx
@@ -6,8 +6,7 @@ const ImageSlider: React.FC = () => {
       <Image
         src="https://res.cloudinary.com/dj4jijw2a/image/upload/v1728902747/webphome_1_optimized_10_mvqf3i.avif"
         alt="Hero Image"
-        height={800}
-        width={1200}
+        fill
         priority
         sizes="(max-width: 450px) 100vw, (max-width: 1024px) 50vw, 1200px"
         className="w-full h-full object-cover rounded-2xl"
