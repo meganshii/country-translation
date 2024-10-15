f9e9ba0d0466c85192e38ac4b0aa8f13ba1393f6
kanha-9770 <app3.nessco@gmail.com>
2024-10-15 13:38:16 +0530

    again removing image

--- src/components/ui/ImageSlider.tsx
+++ src/components/ui/ImageSlider.tsx
@@ -3,15 +3,7 @@ import Image from "next/image";
 const ImageSlider: React.FC = () => {
   return (
     <div className="relative w-full mx-auto h-full">
-      <Image
-        src="https://assets.nesscoindustries.com/public/assets/nav_machine/PaperBagMachine%20.png"
-        alt="Hero Image"
-        priority
-        height={200}
-        width={200}
-        sizes="(max-width: 650px) 10vw, (max-width: 1024px) 50vw, 1200px"
-        className="w-full h-full object-cover rounded-2xl"
-        />
+      hellow world
     </div>
   );
 };
