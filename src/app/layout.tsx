d86a7797b47497bce4dfd47ffec8e680b6b01cd6
kanha-9770 <app3.nessco@gmail.com>
2024-10-16 09:27:53 +0530

    removed script from layout for mobile perfomace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import NavbarDemo from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-import Script from "next/script";
 
 // Initialize the fonts with the correct configurations and use "font-display: swap"
 const inter = Inter({
@@ -24,50 +23,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Asynchronously load the external PageSense script */}
-        <Script
-          src="https://cdn.pagesense.io/js/nesscoindia/ff3c25fdacd845338fcb5edd343fcde6.js"
-          strategy="lazyOnload"
-        />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16529601205"
-        ></script>
-        {/* Google tag (gtag.js)  */}
-        <Script id="gtag-init" strategy="beforeInteractive">
-          {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'AW-16529601205');
-  `}
-        </Script>
+     
       </head>
       <body className={`${inter.variable} ${poppins.variable}`}>
         <NavbarDemo />
         {children}
         <Footer />
 
-        {/* Zoho SalesIQ Chat Script */}
-        <Script id="zsiqchat" strategy="lazyOnload">
-          {`
-            var $zoho = $zoho || {};
-            $zoho.salesiq = $zoho.salesiq || {
-              widgetcode: "siq57ecdd6785594ae3a0a956b5169f571c3e9a79d85694cb61eae8437cb511a908",
-              values: {},
-              ready: function() {}
-            };
-            var d = document;
-            var s = d.createElement("script");
-            s.type = "text/javascript";
-            s.id = "zsiqscript";
-            s.defer = true;
-            s.src = "https://salesiq.zohopublic.com/widget";
-            var t = d.getElementsByTagName("script")[0];
-            t.parentNode.insertBefore(s, t);
-          `}
-        </Script>
+      
       </body>
     </html>
   );
