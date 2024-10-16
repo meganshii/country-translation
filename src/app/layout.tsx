3e04ec5edf220ff373f17795b1ae80b8f2a9af06
kanha-9770 <app3.nessco@gmail.com>
2024-10-16 09:55:36 +0530

    strategy=worker is used for testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,14 +29,14 @@ export default function RootLayout({
         {/* Asynchronously load the external PageSense script */}
         <Script
           src="https://cdn.pagesense.io/js/nesscoindia/ff3c25fdacd845338fcb5edd343fcde6.js"
-          strategy="lazyOnload"
+          strategy="worker"
         />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-16529601205"
         ></script>
         {/* Google tag (gtag.js)  */}
-        <Script id="gtag-init" strategy="lazyOnload">
+        <Script id="gtag-init" strategy="worker">
           {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
@@ -52,7 +52,7 @@ export default function RootLayout({
         <Footer />
 
         {/* Zoho SalesIQ Chat Script */}
-        <Script id="zsiqchat" strategy="lazyOnload">
+        <Script id="zsiqchat" strategy="worker">
           {`
             var $zoho = $zoho || {};
             $zoho.salesiq = $zoho.salesiq || {
