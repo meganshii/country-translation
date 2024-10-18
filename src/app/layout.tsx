0d7286eb63fe76984e9a48949a02138df32d425b
kanha-9770 <app3.nessco@gmail.com>
2024-10-18 13:18:09 +0530

    added footer and navbar dynamically

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import NavLayout from "@/components/Navbar/NavLayout";
-
+import dynamic from "next/dynamic";
+const NavLayout=dynamic(()=>import("@/components/Navbar/NavLayout"));
+const FooterLayout=dynamic(()=>import("@/components/Footer/FooterLayout"));
 // Initialize the fonts with the correct configurations and use "font-display: swap"
 const inter = Inter({
   subsets: ["latin"],
@@ -46,6 +47,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${poppins.variable}`}>
         <NavLayout />
         {children}
+        <FooterLayout/>
         {/* Zoho SalesIQ Chat Script */}
         <Script id="zsiqchat" strategy="lazyOnload">
           {`
